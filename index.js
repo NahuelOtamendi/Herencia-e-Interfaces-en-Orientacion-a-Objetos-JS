551465c591cb90cea7fe46564d06342b7f57b61a
Nahuel Otamendi <nahuotamendi@gmail.com>
2023-05-23 08:17:47 -0300

    Se creo archivo cuentaAhorro.js para crear cajas de ahorros.

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 /*Importación de clases*/
-import {Cliente} from './Cliente.js'
-import {CuentaCorriente} from './CuentaCorriente.js';
+import { Cliente } from "./Cliente.js";
+import { CuentaCorriente } from "./CuentaCorriente.js";
+import { CuentaAhorro } from "./cuentaAhorro.js";
 
-const cliente = new Cliente('Leonardo','13804050','123224');
-const cliente2 = new Cliente('María','16979808','8989');
+const cliente1 = new Cliente("Leonardo", "13804050", "123224");
+const cliente2 = new Cliente("María", "16979808", "8989");
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+const cuentaDeLeonardo = new CuentaCorriente(cliente1, "1", "001");
+const cuentaDeMaria = new CuentaCorriente(cliente2, "2", "002");
 
-let saldo = cuentaDeLeonardo.verSaldo(); 
+const cuentaAhorroLeonardo = new CuentaAhorro(cliente1, "9985", "001", 0);
+const cuentaAhorroMaria = new CuentaAhorro(cliente2, "9986", "002", 0);
 
-saldo = cuentaDeLeonardo.depositoEnCuenta(150);
-cuentaDeLeonardo.transferirParaCuenta(parametroValor,cuentaDeMaria);
-
-const saldoMaria = cuentaDeMaria.verSaldo();
-
-console.log('Cuenta de Maria',cuentaDeMaria);
-
-console.log('El Saldo actual (cuentaMaria) '+saldoMaria);
-
-const saldoLeonardo = cuentaDeLeonardo.verSaldo();
-console.log('El Saldo actual (cuentaLeonardo) '+saldoLeonardo);
-console.log(CuentaCorriente.cantidadCuentas);
-
-console.log(CuentaCorriente.cantidadCuentas);
-
-
-
-
+console.log(cuentaDeLeonardo);
+console.log(cuentaAhorroLeonardo);
