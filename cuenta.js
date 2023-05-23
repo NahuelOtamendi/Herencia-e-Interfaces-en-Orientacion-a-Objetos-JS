5e39f1f86f538e82b779b5885973912b7730ce88
Nahuel Otamendi <nahuotamendi@gmail.com>
2023-05-23 08:41:13 -0300

    Se unificaron las clases en un solo archivo JS para evitar la repeticion de codigo para asi hacer mas facil la mantenibilidad del mismo.

--- cuenta.js
+++ cuenta.js
@@ -1,26 +1,11 @@
-import { Cliente } from "./Cliente.js";
-
-export class CuentaCorriente {
+export class Cuenta {
   #cliente;
-  numero;
-  agencia;
   #saldo;
-  static cantidadCuentas = 0;
-
-  set cliente(valor) {
-    if (valor instanceof Cliente) this.#cliente = valor;
-  }
-
-  get cliente() {
-    return this.#cliente;
-  }
-
-  constructor(cliente, numero, agencia) {
-    this.cliente = cliente;
+  constructor(cliente, numero, agencia, saldo) {
+    this.#cliente = cliente;
     this.numero = numero;
     this.agencia = agencia;
-    this.#saldo = 0;
-    CuentaCorriente.cantidadCuentas++;
+    this.#saldo = saldo;
   }
 
   depositoEnCuenta(valor) {
--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 /*Importación de clases*/
 import { Cliente } from "./Cliente.js";
-import { CuentaCorriente } from "./CuentaCorriente.js";
-import { CuentaAhorro } from "./cuentaAhorro.js";
+import { Cuenta } from "./cuenta.js";
+//import { CuentaCorriente } from "./CuentaCorriente.js";
+//import { CuentaAhorro } from "./cuentaAhorro.js";
 
 const cliente1 = new Cliente("Leonardo", "13804050", "123224");
 const cliente2 = new Cliente("María", "16979808", "8989");
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente1, "1", "001");
-const cuentaDeMaria = new CuentaCorriente(cliente2, "2", "002");
+const cuentaDeLeonardo = new Cuenta(cliente1, "1", "001", 0);
+const cuentaDeMaria = new Cuenta(cliente2, "2", "002");
 
-const cuentaAhorroLeonardo = new CuentaAhorro(cliente1, "9985", "001", 0);
-const cuentaAhorroMaria = new CuentaAhorro(cliente2, "9986", "002", 0);
+const cuentaAhorroLeonardo = new Cuenta(cliente1, "9985", "001", 0);
 
 console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.depositoEnCuenta(150);
+console.log(cuentaDeLeonardo.verSaldo());
+cuentaDeLeonardo.retirarDeCuenta(70);
+console.log(cuentaDeLeonardo.verSaldo());
+
 console.log(cuentaAhorroLeonardo);
+cuentaAhorroLeonardo.depositoEnCuenta(200);
+console.log(cuentaAhorroLeonardo.verSaldo());
+cuentaAhorroLeonardo.retirarDeCuenta(90);
+console.log(cuentaAhorroLeonardo.verSaldo());
